edec1cf920dc80d6cf6939fd1ab56914233a7fd9
Gurshaan Daula <gurshaandaula@gmail.com>
2025-10-15 23:35:51 -0700

    added aiven

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")), // 👈 required by Aiven
+  },
+  connectionLimit: 10,
 });
 
 module.exports = pool;
